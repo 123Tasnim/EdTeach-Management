7eefd6890412c8a3ca69d520f59f8aa596afbc75
Sumiya Hur Tasnim <145317865+123Tasnim@users.noreply.github.com>
2024-12-21 00:25:50 +0600

    Add files via upload

---
+++ SqlDbDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdTeach_Management.Models
+{
+    public class SqlDbDataAccess
+    {
+        private const string V = @"Data Source=HUJAIFA\SQLEXPRESS;Initial Catalog=EdTechMG7;Integrated Security=True";
+        const string Connectionstring = V;
+
+
+        public SqlCommand GetCommand(string query)
+        {
+            var connection = new SqlConnection(Connectionstring);
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Connection = connection;
+            return cmd;
+        }
+    }
+}
